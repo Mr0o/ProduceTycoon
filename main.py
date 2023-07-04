407e0fd80ca5339e1085994f88e8edd436590154
Lukas Tackett <luetack@gmail.com>
2023-07-03 21:12:31 -0400

    Added a moving background to look around

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+import sys
 from random import randint
 
 pygame.init()
+
 
 class Game:
     def __init__(self):
@@ -11,10 +13,18 @@ class Game:
         pygame.display.set_caption('Game')
 
         self.background_rgb = (randint(0, 255), randint(0, 255), randint(0, 255))
+        self.keys = []
+        self.background_x = -1000
+        self.background_y = -1000
+        self.display_scroll = [0, 0]
+        self.background_height = 2001
+        self.background_width = 2001
+        self.background_box = pygame.Rect((self.background_x - self.display_scroll[0], self.background_y - self.display_scroll[1]), (self.background_width, self.background_height))
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                sys.exit()
                 self.running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.background_rgb = (randint(0, 255), randint(0, 255), randint(0, 255))
@@ -24,12 +34,33 @@ class Game:
                 if event.key == pygame.K_SPACE:
                     self.background_rgb = (randint(0, 255), randint(0, 255), randint(0, 255))
 
+        self.keys = pygame.key.get_pressed()
+        # Each key moves the background the oposite direction as we want our character to move.
+        # Scroll left
+        if self.keys[pygame.K_a]:
+            self.display_scroll[0] -= 20
+        # Scroll right
+        if self.keys[pygame.K_d]:
+            self.display_scroll[0] += 20
+        # Scroll down
+        if self.keys[pygame.K_w]:
+            self.display_scroll[1] -= 20
+        # Scroll Up
+        if self.keys[pygame.K_s]:
+            self.display_scroll[1] += 20
+        
+        # redrawling background image and border
+        self.background_box = pygame.Rect((self.background_x - self.display_scroll[0], self.background_y - self.display_scroll[1]), (self.background_width, self.background_height))
+
     def update(self):
         pass
 
     def draw(self):
-        self.screen.fill(self.background_rgb)
+        self.screen.fill((0,0,0))
 
+        # Creating a background and a border
+        pygame.draw.rect(self.screen, (0, 255, 0), self.background_box)
+        pygame.draw.rect(self.screen, (255, 0, 0), self.background_box, 2)
         # load font
         font = pygame.font.SysFont('Arial', 30)
 
@@ -48,6 +79,10 @@ class Game:
         # draw help text
         text = font.render("Press SPACE to change background color", True, (0, 0, 0))
         self.screen.blit(text, (0, 150))
+        
+        # draw position (x, y)
+        text = font.render("(" + str(self.background_box.x) + ", " + str(self.background_box.y) + ")", True, (0, 0, 0))
+        self.screen.blit(text, (0, 180))
 
         pygame.display.update()
 
