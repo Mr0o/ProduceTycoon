ec3f839ae88faac990ecffc04e2d2bfe834a9e63
Lukas Tackett <luetack@gmail.com>
2023-07-05 13:06:14 -0400

    updated character size

--- character.py
+++ character.py
@@ -5,7 +5,7 @@ class Character:
         self.screen = screen
         self.x = x
         self.y = y
-        self.width = 20
+        self.width = 30
         self.height = 45
         self.animation_images = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (120, 120, 120)]
         self.animation_count = 0
@@ -16,16 +16,16 @@ class Character:
         # Each key moves the background the oposite direction that we want our character to move.
         # Scroll left
         if self.keys[pygame.K_a]:
-            self.x -= 20
+            self.x += 20
         # Scroll right
         if self.keys[pygame.K_d]:
-            self.x += 20
+            self.x -= 20
         # Scroll down
         if self.keys[pygame.K_w]:
-            self.y -= 20
+            self.y += 20
         # Scroll Up
         if self.keys[pygame.K_s]:
-            self.y += 20
+            self.y -= 20
 
         # get relative mouse position
         self.rel_mouse_pos = pygame.mouse.get_rel()
@@ -34,9 +34,10 @@ class Character:
         # if left mouse button is pressed
         if self.mouse_buttons[0]:
             # scroll background
-            self.x -= self.rel_mouse_pos[0]
-            self.y -= self.rel_mouse_pos[1]
+            self.x += self.rel_mouse_pos[0]
+            self.y += self.rel_mouse_pos[1]
         
+        # counting the frames once we get to 15 we reset to 0 back to the first image in out animation
         if (self.animation_count >= 15):
             self.animation_count = 0
         self.animation_count += 1
