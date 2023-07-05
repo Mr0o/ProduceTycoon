0a798fc6f935cce42a19ce11eac6545743a05384
Owen Smith <owen.smith1201@gmail.com>
2023-07-05 14:41:01 -0400

    Removed unused mouse input handling

--- character.py
+++ character.py
@@ -26,16 +26,6 @@ class Character:
         # Scroll Up
         if self.keys[pygame.K_s]:
             self.y -= 20
-
-        # get relative mouse position
-        self.rel_mouse_pos = pygame.mouse.get_rel()
-        # get mouse buttons
-        self.mouse_buttons = pygame.mouse.get_pressed()
-        # if left mouse button is pressed
-        if self.mouse_buttons[0]:
-            # scroll background
-            self.x += self.rel_mouse_pos[0]
-            self.y += self.rel_mouse_pos[1]
         
         # counting the frames once we get to 15 we reset to 0 back to the first image in out animation
         if (self.animation_count >= 15):
