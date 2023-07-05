14649a826a38e2036b4bb0e9519626186e18e7c2
Lukas Tackett <luetack@gmail.com>
2023-07-05 18:41:14 -0400

    imported movement file

--- background.py
+++ background.py
@@ -1,5 +1,7 @@
 import pygame
 
+from movement import Movement
+
 class Background:
     def __init__(self, screen: pygame.Surface, x: int, y: int):
         self.screen = screen
@@ -12,30 +14,8 @@ class Background:
         self.background_img = pygame.transform.scale(self.background_img, (self.width, self.height))
 
     def events(self):
-        self.keys = pygame.key.get_pressed()
-        # Each key moves the background the oposite direction that we want our character to move.
-        # Scroll left
-        if self.keys[pygame.K_a]:
-            self.x += 20
-        # Scroll right
-        if self.keys[pygame.K_d]:
-            self.x -= 20
-        # Scroll down
-        if self.keys[pygame.K_w]:
-            self.y += 20
-        # Scroll Up
-        if self.keys[pygame.K_s]:
-            self.y -= 20
-        
-        # get relative mouse position
-        self.rel_mouse_pos = pygame.mouse.get_rel()
-        # get mouse buttons
-        self.mouse_buttons = pygame.mouse.get_pressed()
-        # if left mouse button is pressed
-        if self.mouse_buttons[0]:
-            # scroll background
-            self.x += self.rel_mouse_pos[0]
-            self.y += self.rel_mouse_pos[1]
+        self.x = Movement.check_if_move(self.x, self.y)[0]
+        self.y = Movement.check_if_move(self.x, self.y)[1]
             
     def update(self):
         # Updatig background and border
--- guest.py
+++ guest.py
@@ -1,5 +1,7 @@
 import pygame
 
+from movement import Movement
+
 class Guest(object):
     def __init__(self, screen: pygame.Surface, x: int, y: int):
         self.screen = screen
@@ -9,40 +11,19 @@ class Guest(object):
         self.height = 45
         self.moving_left = False
         self.moving_right = False
-        self.animation_images = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (120, 120, 120)]
+        self.animation_images = [(0, 50, 0), (0, 100, 0), (0, 150, 0), (0, 200, 0), (0, 255, 0)]
+        self.animation_images_left = [(50, 0, 0), (100, 0, 0), (150, 0, 0), (200, 0, 0), (255, 0, 0)]
         self.animation_count = 0
         self.rect = pygame.Rect((self.x, self.y), (self.width, self.height))
 
     def events(self):
         # counting the frames once we get to 39 we reset to 0 back to the first image in out animation
-        if (self.animation_count >= 39):
+        if (self.animation_count >= 49):
             self.animation_count = 0
         self.animation_count += 1
 
-        self.keys = pygame.key.get_pressed()
-        # Each key moves the background the oposite direction that we want our character to move.
-        # Scroll left
-        if self.keys[pygame.K_a]:
-            self.x += 20
-        # Scroll right
-        if self.keys[pygame.K_d]:
-            self.x -= 20
-        # Scroll down
-        if self.keys[pygame.K_w]:
-            self.y += 20
-        # Scroll Up
-        if self.keys[pygame.K_s]:
-            self.y -= 20
-        
-        # get relative mouse position
-        self.rel_mouse_pos = pygame.mouse.get_rel()
-        # get mouse buttons
-        self.mouse_buttons = pygame.mouse.get_pressed()
-        # if left mouse button is pressed
-        if self.mouse_buttons[0]:
-            # scroll background
-            self.x += self.rel_mouse_pos[0]
-            self.y += self.rel_mouse_pos[1]
+        self.x = Movement.check_if_move(self.x, self.y)[0]
+        self.y = Movement.check_if_move(self.x, self.y)[1]
 
         # Add flipping to images to show which direction we are moving
     
