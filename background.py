b70eb440f47021e39aabc863f01baf63b1afeb7c
Lukas Tackett <luetack@gmail.com>
2023-07-05 13:05:59 -0400

    Fixed background movement

--- background.py
+++ background.py
@@ -13,16 +13,16 @@ class Background:
         # Each key moves the background the oposite direction that we want our character to move.
         # Scroll left
         if self.keys[pygame.K_a]:
-            self.x -= 20
+            self.x += 20
         # Scroll right
         if self.keys[pygame.K_d]:
-            self.x += 20
+            self.x -= 20
         # Scroll down
         if self.keys[pygame.K_w]:
-            self.y -= 20
+            self.y += 20
         # Scroll Up
         if self.keys[pygame.K_s]:
-            self.y += 20
+            self.y -= 20
         
         # get relative mouse position
         self.rel_mouse_pos = pygame.mouse.get_rel()
@@ -31,8 +31,8 @@ class Background:
         # if left mouse button is pressed
         if self.mouse_buttons[0]:
             # scroll background
-            self.x -= self.rel_mouse_pos[0]
-            self.y -= self.rel_mouse_pos[1]
+            self.x += self.rel_mouse_pos[0]
+            self.y += self.rel_mouse_pos[1]
     def update(self):
         self.events()
         # Updatig background and border
