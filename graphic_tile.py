2e5098317aeab98b3edcd92cd099dabebfef11c3
Lukas Tackett <luetack@gmail.com>
2023-07-06 10:20:10 -0400

    added graphic tile class

---
+++ graphic_tile.py
@@ -0,0 +1,13 @@
+import pygame
+
+from tile import Tile
+
+class Graphic_Tile(Tile):
+    def __init__(self, screen: pygame.Surface, x: int, y: int, size: int, tile_img: pygame.Surface):
+        super().__init__(screen, x, y, size)
+        # scale image
+        self.tile_img = pygame.transform.scale(tile_img, (self.size, self.size))
+    def draw(self):
+        self.screen.blit(self.tile_img, (self.x, self.y))
+
+
