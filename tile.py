e5ea6ba14ba85c9a7fab5f909da8ed6624e79c90
Owen Smith <owen.smith1201@gmail.com>
2023-07-06 18:47:39 -0400

    Begun implementing types to the tile class

--- tile.py
+++ tile.py
@@ -1,12 +1,37 @@
 import pygame
+from enum import Enum
+
+WALKABLE_TILE_IMG = pygame.image.load('./bg.jpg')
+INTERACTABLE_TILE_IMG = pygame.image.load('./bg.jpg')
+BOUNDARY_TILE_IMG = pygame.image.load('./Barrier.png')
+
+class Type(Enum):
+    WALKABLE = 1
+    INTERACTABLE = 2
+    BOUNDARY = 3
 
 class Tile():
-    def __init__(self, screen: pygame.Surface, x: int, y: int, size: int):
+    def __init__(self, screen: pygame.Surface, x: int, y: int, size: int, type: Type = Type.WALKABLE):
         self.screen = screen
         self.x = x
         self.y = y
         self.size = size
-        #self.tile_img = pygame.image.load('./bg.jpg')
+
+        self.type = type
+        
+        self.tile_img = None
+        if self.type == Type.WALKABLE:
+            self.tile_img = WALKABLE_TILE_IMG
+        elif self.type == Type.INTERACTABLE:
+            self.tile_img = INTERACTABLE_TILE_IMG
+        elif self.type == Type.BOUNDARY:
+            self.tile_img = BOUNDARY_TILE_IMG
+        else:
+            raise Exception("Invalid tile type")
+        
+        # scale image to size
+        self.tile_img = pygame.transform.scale(self.tile_img, (self.size, self.size))
+
         self.rect = pygame.Rect((self.x, self.y), (self.size, self.size))
 
     def update(self, x:int, y:int):
