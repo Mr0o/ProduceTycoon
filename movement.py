586a1a54fa422bf200fd36472636a063a5f38b58
Owen Smith <owen.smith1201@gmail.com>
2023-07-05 23:49:44 -0400

    Refactored check_if_move from a class method to a function

--- movement.py
+++ movement.py
@@ -1,32 +1,30 @@
 import pygame
 
-class Movement:
-    @classmethod
-    def check_if_move(cls, x: int, y: int):
-        keys = pygame.key.get_pressed()
-        # Each key moves the background the oposite direction that we want our character to move.
-        # Scroll left
-        if keys[pygame.K_a]:
-            x += 20
-        # Scroll right
-        if keys[pygame.K_d]:
-            x -= 20
-        # Scroll down
-        if keys[pygame.K_w]:
-            y += 20
-        # Scroll Up
-        if keys[pygame.K_s]:
-            y -= 20
-        
-        # get relative mouse position
-        rel_mouse_pos = pygame.mouse.get_rel()
-        # get mouse buttons
-        mouse_buttons = pygame.mouse.get_pressed()
-        # if left mouse button is pressed
-        if mouse_buttons[0]:
-            # scroll background
-            x += rel_mouse_pos[0]
-            y += rel_mouse_pos[1]
+def check_if_move(x: int, y: int):
+    keys = pygame.key.get_pressed()
+    # Each key moves the background the oposite direction that we want our character to move.
+    # Scroll left
+    if keys[pygame.K_a]:
+        x += 20
+    # Scroll right
+    if keys[pygame.K_d]:
+        x -= 20
+    # Scroll down
+    if keys[pygame.K_w]:
+        y += 20
+    # Scroll Up
+    if keys[pygame.K_s]:
+        y -= 20
+    
+    # get relative mouse position
+    rel_mouse_pos = pygame.mouse.get_rel()
+    # get mouse buttons
+    mouse_buttons = pygame.mouse.get_pressed()
+    # if left mouse button is pressed
+    if mouse_buttons[0]:
+        # scroll background
+        x += rel_mouse_pos[0]
+        y += rel_mouse_pos[1]
 
-        # after all movement has been checked returns updated x and y positions
-        return (x, y)
+    # after all movement has been checked returns updated x and y positions
+    return (x, y)
