4aa80428d7ef40f2798ba7e2b92dbd128b638ab6
Owen Smith <owen.smith1201@gmail.com>
2023-07-05 23:48:50 -0400

    Created grid of tiles to make up the background

--- background.py
+++ background.py
@@ -1,22 +1,49 @@
 import pygame
 
-from movement import Movement
+from movement import check_if_move
+from tile import Tile
 
 class Background:
     def __init__(self, screen: pygame.Surface, x: int, y: int):
         self.screen = screen
         self.x = x
         self.y = y
-        self.size = 20
-        self.background_img = pygame.transform.scale(pygame.image.load('./bg.jpg'), (self.size, self.size))
+
+        # create grid of tiles
+        self.rows = 20
+        self.col = 20
+        self.background_grid: list[list[Tile]] = []
+        self.background_starting_pos = 0
+        self.tile_size = 20
+        for i in range(self.rows):
+            col: list[Tile] = []
+            for j in range(self.col):
+                col.append(Tile(self.screen, self.background_starting_pos - 20 * i,self.background_starting_pos - 20 * i, self.tile_size))
+                self.background_grid.append(col)
 
     def events(self):
-        self.x, self.y = Movement.check_if_move(self.x, self.y)
-            
+        # get relative mouse position
+        rel_mouse_pos = pygame.mouse.get_rel()
+        # get mouse buttons
+        mouse_buttons = pygame.mouse.get_pressed()
+        # if left mouse button is pressed
+        if mouse_buttons[0]:
+            # scroll background
+            self.x += rel_mouse_pos[0]
+            self.y += rel_mouse_pos[1]
+        
+        for i in range(self.rows):
+            for j in range(self.col):
+                self.background_grid[i][j].events()
+
     def update(self):
-        # Updatig background and border
         pass
 
     def draw(self):
-        # Drawling background and a border
-       self.screen.blit(self.background_img, (self.x, self.y))
+        # drawing background
+        for i in range(self.rows):
+            for j in range(self.col):
+                self.background_grid[i][j].draw()
+
+        
+
