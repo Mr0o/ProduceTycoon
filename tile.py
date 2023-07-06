0eaec15622a58740f2206bb44d54ce4dd51ba66f
Lukas Tackett <luetack@gmail.com>
2023-07-06 12:12:58 -0400

    empty tiles now draw white

--- tile.py
+++ tile.py
@@ -7,13 +7,13 @@ class Tile():
         self.y = y
         self.size = size
         #self.tile_img = pygame.image.load('./bg.jpg')
-        #self.tile_img = pygame.Rect((self.x, self.y), (self.size, self.size))
+        self.rect = pygame.Rect((self.x, self.y), (self.size, self.size))
 
     def update(self, x:int, y:int):
         self.x += x
         self.y += y
+        self.rect = pygame.Rect((self.x, self.y), (self.size, self.size))
         
     def draw(self):
-        pass
-        #pygame.draw.rect(self.screen, (255, 255, 255), self.tile_img)
+        pygame.draw.rect(self.screen, (255, 255, 255), self.rect)
         #self.screen.blit(self.tile_img, (self.x, self.y))
