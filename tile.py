7797ba579866563216d199103b5b78b1fa3bc513
Lukas Tackett <luetack@gmail.com>
2023-07-06 10:20:22 -0400

    updated tile class

--- tile.py
+++ tile.py
@@ -1,12 +1,13 @@
 import pygame
 
 class Tile():
-    def __init__(self, screen: pygame.Surface, x: int, y: int, size: int, tile_img: pygame.Surface):
+    def __init__(self, screen: pygame.Surface, x: int, y: int, size: int):
         self.screen = screen
         self.x = x
         self.y = y
         self.size = size
-        self.tile_img = tile_img
+        self.tile_img = pygame.Rect((self.x, self.y), (self.size, self.size))
         
-        # scale image
-        self.tile_img = pygame.transform.scale(self.tile_img, (self.size, self.size))
+    def draw(self):
+        pygame.draw.rect(self.screen, (0, 0, 0), self.tile_img)
+        #self.screen.blit(self.background_grid.tile_img, (self.x, self.y))
