0069c76fe31bc37d717cf956b7783362dd7fb70d
Lukas Tackett <luetack@gmail.com>
2023-07-05 21:32:55 -0400

    got rid of outside red border

--- background.py
+++ background.py
@@ -8,18 +8,15 @@ class Background:
         self.x = x
         self.y = y
         self.size = 20
-        self.background_box = pygame.Rect((self.x, self.y), (self.size, self.size))
-        self.background_img = pygame.image.load('./bg.jpg')
-        self.background_img = pygame.transform.scale(self.background_img, (self.size, self.size))
+        self.background_img = pygame.transform.scale(pygame.image.load('./bg.jpg'), (self.size, self.size))
 
     def events(self):
         self.x, self.y = Movement.check_if_move(self.x, self.y)
             
     def update(self):
         # Updatig background and border
-        self.background_box = pygame.Rect((self.x, self.y), (self.size, self.size))
+        pass
 
     def draw(self):
         # Drawling background and a border
-        self.screen.blit(self.background_img, (self.x, self.y))
-        pygame.draw.rect(self.screen, (255, 0, 0), self.background_box, 2)
+       self.screen.blit(self.background_img, (self.x, self.y))
