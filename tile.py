3792ee0a58de6c28002564ea330792bf894431d5
Owen Smith <owen.smith1201@gmail.com>
2023-07-06 00:31:25 -0400

    Tile no longer has an events, update or draw method

--- tile.py
+++ tile.py
@@ -7,21 +7,6 @@ class Tile:
         self.y = y
         self.size = size
         self.tile_img = tile_img
-
-    def events(self):
-        # get relative mouse position
-        rel_mouse_pos = pygame.mouse.get_rel()
-        # get mouse buttons
-        mouse_buttons = pygame.mouse.get_pressed()
-        # if left mouse button is pressed
-        if mouse_buttons[0]:
-            # scroll background
-            self.x += rel_mouse_pos[0]
-            self.y += rel_mouse_pos[1]
-            
-    def update(self):
-        pass
-
-    def draw(self):
-        self.screen.blit(self.tile_img, (self.x, self.y))
-        print(self.x, self.y)
+        
+        # scale image
+        self.tile_img = pygame.transform.scale(self.tile_img, (self.size, self.size))
