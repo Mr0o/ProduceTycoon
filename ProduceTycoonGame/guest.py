59fd38ca869359afef237df277ce487210aef992
Owen Smith <owen.smith1201@gmail.com>
2023-07-08 17:48:05 -0400

    Removed movement related stuff

--- ProduceTycoonGame/guest.py
+++ ProduceTycoonGame/guest.py
@@ -1,22 +1,21 @@
 import pygame
 import random
 
-from ProduceTycoonGame.functions import changeTile
+from ProduceTycoonGame.functions import changeTile, inputMovement
 from ProduceTycoonGame.tileMap import TileMap
 from ProduceTycoonGame.tile import Tile, Type
 
 class Guest():
     def __init__(self, screen: pygame.Surface, tile: Tile, tileMap: TileMap):
+        self.x = tile.x
+        self.y = tile.y
+
         self.screen = screen
         self.tile = tile
         self.tileMap = tileMap
 
-        global id
-        self.previousTile = None
-        self.size = self.tile.size
-        self.movements = ["right", "right", "right", "down", "down"]
-        self.movementCount = 0
-        self.movementCountMax = len(self.movements) * 60 - 1
+        self.size = 25
+
         self.animationImages = [(0, 50, 0), (0, 100, 0), (0, 150, 0), (0, 200, 0), (0, 255, 0)]
         self.animationCount = 0
         self.rect = pygame.Rect((self.tile.x, self.tile.y), (self.size, self.size))
@@ -26,34 +25,19 @@ class Guest():
         if self.animationCount >= 49:
             self.animationCount = 0
         self.animationCount += 1
-        if self.movementCount >= self.movementCountMax:
-            self.movementCount = 0
-            self.movements = newPath(int(random.random() * id))
-        self.movementCount += 1
-        
 
-        #saves the previous tile you were on
-        self.previousTile = self.tile
-
-        # changing tile
-        self.tile = changeTile(self, self.movements[self.movementCount//60])
-        self.movements[self.movementCount//60] = "moved"
-
-        # checks if the tile you move to is a boundary tile if it is you move back to previous tile
-        if self.tile.type == Type.BOUNDARY:
-            self.tile = self.previousTile
-        #self.x, self.y = inputMovement(self.x, self.y)
-        
+        self.x, self.y = inputMovement(self.x, self.y)
 
         # Add flipping to images to show which direction we are moving
 
     
     def update(self):   
         # Updating guest
-        self.movementCountMax = len(self.movements) * 60 - 1
         self.rect = pygame.Rect((self.tile.x, self.tile.y), (self.size, self.size))
 
     def draw(self):
         # Drawling guest
         pygame.draw.rect(self.screen, self.animationImages[self.animationCount//10], self.rect)
+
+        
     
