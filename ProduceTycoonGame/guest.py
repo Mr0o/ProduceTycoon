7b78f442b3a0eebfa7100f7b33891af96af43625
Owen Smith <owen.smith1201@gmail.com>
2023-07-09 19:55:03 -0400

    Guests use the name "Guest" and a have a unique id

--- ProduceTycoonGame/guest.py
+++ ProduceTycoonGame/guest.py
@@ -5,7 +5,14 @@ from ProduceTycoonGame.vectors import Vector
 from ProduceTycoonGame.tile import Tile
 from ProduceTycoonGame.person import Person
 
+# global
+id = 0
+
 class Guest(Person):
+    def __init__(self, screen: pygame.Surface, pos: Vector, name: str = "Guest"):
+        global id; id += 1
+        super().__init__(screen, pos, id, name)
+
     def setTarget(self, targetTile: Tile):
         if targetTile != None:
             # create a vector from the guest to the target tile
@@ -14,3 +21,14 @@ class Guest(Person):
 
         # set the target tile
         self.targetTile = targetTile
+
+    # draw
+    def draw(self):
+        super().draw()
+        
+        # load font
+        debugFont = pygame.font.SysFont('Arial', 15, bold=True)
+
+        # draw name
+        nameText = debugFont.render(self.name, True, (0, 255, 0))
+        self.screen.blit(nameText, (self.pos.x, self.pos.y - nameText.get_height()))
