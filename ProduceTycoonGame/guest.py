28037635b127ea235ddc0b545bb34666928ce7eb
Owen Smith <owen.smith1201@gmail.com>
2023-07-08 20:09:09 -0400

    Added setTarget method to make guest move towards a target tile

--- ProduceTycoonGame/guest.py
+++ ProduceTycoonGame/guest.py
@@ -1,22 +1,24 @@
 import pygame
 import random
 
+from ProduceTycoonGame.vectors import Vector
 from ProduceTycoonGame.functions import inputMovement
 from ProduceTycoonGame.tileMap import TileMap
 from ProduceTycoonGame.tile import Tile, Type
 
 class Guest():
-    def __init__(self, screen: pygame.Surface, tile: Tile):
-        self.pos = tile.pos.copy()
-
+    def __init__(self, screen: pygame.Surface, pos: Vector):
         self.screen = screen
-        self.tile = tile
+        self.pos = pos
+        self.size = 50
 
-        self.size = 25
+        self.mov = Vector(0, 0)
+        self.targetTile: Tile = None
 
-        self.animationImages = [(0, 50, 0), (0, 100, 0), (0, 150, 0), (0, 200, 0), (0, 255, 0)]
+        self.animationImages = [
+            (0, 50, 0), (0, 100, 0), (0, 150, 0), (0, 200, 0), (0, 255, 0)]
         self.animationCount = 0
-        self.rect = pygame.Rect((self.tile.pos.x, self.tile.pos.y), (self.size, self.size))
+        self.rect = pygame.Rect((self.pos.x, self.pos.y), (self.size, self.size))
 
     def events(self):
         # counting the frames once we get to 49 we reset to 0 back to the first image in out animation
@@ -28,14 +30,30 @@ class Guest():
 
         # Add flipping to images to show which direction we are moving
 
-    
-    def update(self):   
+    def update(self):
         # Updating guest
-        self.rect = pygame.Rect((self.tile.pos.x, self.tile.pos.y), (self.size, self.size))
+        # move towards target tile
+        if self.targetTile != None:
+            # check if rects are colliding
+            if self.rect.colliderect(self.targetTile.rect):
+                self.targetTile = None
+                self.mov = Vector(0, 0)
+            else:
+                self.pos.add(self.mov)
+
+        self.rect = pygame.Rect((self.pos.x, self.pos.y), (self.size, self.size))
+
 
     def draw(self):
         # Drawing guest
-        pygame.draw.rect(self.screen, self.animationImages[self.animationCount//10], self.rect)
+        pygame.draw.rect(
+            self.screen, self.animationImages[self.animationCount//10], self.rect)
 
-        
-    
+    def setTarget(self, targetTile: Tile):
+        if targetTile != None:
+            # create a vector from the guest to the target tile
+            self.mov = Vector(targetTile.pos.x - self.pos.x, targetTile.pos.y - self.pos.y)
+            self.mov.setMag(5)
+
+        # set the target tile
+        self.targetTile = targetTile
