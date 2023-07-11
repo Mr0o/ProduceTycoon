c19e0c9f482393e7a7fbafb9e1cf267117d35036
Lukas Tackett <luetack@gmail.com>
2023-07-11 18:56:48 -0400

    officially changes tiles to interactables: buggy

--- ProduceTycoonGame/button.py
+++ ProduceTycoonGame/button.py
@@ -1,8 +1,8 @@
 import pygame
 
 from ProduceTycoonGame.vectors import Vector
-from ProduceTycoonGame.tileMap import TileMap
-from ProduceTycoonGame.tile import Tile, Type
+from ProduceTycoonGame.tileMap import TileMap, createStaticTileSurface
+from ProduceTycoonGame.tile import Type
 
 class Button():
     def __init__(self, screen: pygame.Surface, pos: Vector,tileMap: TileMap, text = "3x3"):
@@ -19,9 +19,8 @@ class Button():
 
         self.rect = pygame.Rect(self.pos.x, self.pos.y, self.width, self.height)
         self.interactableRect = pygame.Rect(0, 0, self.screen.get_height() // 25 * 3, self.screen.get_height() // 25 * 3)
-        
     
-    def events(self, mouseClicked: bool):
+    def events(self, mouseClicked: bool, zeroClicked: bool):
         if self.rect.collidepoint(pygame.mouse.get_pos()) and mouseClicked:
             self.color = (0, 123, 255)
             self.showRect = True
@@ -33,18 +32,20 @@ class Button():
                 if tile.rect.collidepoint(pygame.mouse.get_pos()):
                     self.posInteractable.x = tile.pos.x + tile.size / 2 
                     self.posInteractable.y = tile.pos.y + tile.size / 2
-                if self.interactableRect.colliderect(tile.rect) and mouseClicked:
-                    tile.type = Type.INTERACTABLE
-                    print(tile.type)
+                    if self.interactableRect.colliderect(tile.rect):
+                        tile.type = Type.INTERACTABLE
+                        self.tileMap.staticSurface = createStaticTileSurface(self.tileMap.tileMapGrid, self.tileMap.width, self.tileMap.height)
+        if zeroClicked:
+            self.showRect = False
         
 
 
     def update(self):
         self.interactableRect.center = (self.posInteractable.x, self.posInteractable.y)
+        createStaticTileSurface(self.tileMap.tileMapGrid, self.tileMap.width, self.tileMap.height)
         
 
     def draw(self):
-
         objectSize = pygame.font.SysFont('Arial', 15, bold=True)
         text = objectSize.render(self.text, True, (0, 0, 0))
 
