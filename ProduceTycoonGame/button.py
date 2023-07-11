09795445979a8caef7c3727bc1d2103f7bdca274
Lukas Tackett <luetack@gmail.com>
2023-07-11 18:08:02 -0400

    rectagle is now tile dependant

--- ProduceTycoonGame/button.py
+++ ProduceTycoonGame/button.py
@@ -1,17 +1,21 @@
 import pygame
 
 from ProduceTycoonGame.vectors import Vector
+from ProduceTycoonGame.tileMap import TileMap
+from ProduceTycoonGame.tile import Tile, Type
 
 class Button():
-    def __init__(self, screen: pygame.Surface, pos: Vector, text = "3x3"):
+    def __init__(self, screen: pygame.Surface, pos: Vector,tileMap: TileMap, text = "3x3"):
         self.screen = screen
         self.pos = pos
+        self.tileMap = tileMap
         self.text = text
 
         self.width = 21
         self.height = 20
         self.color = (0, 0, 255)
         self.showRect = False
+        self.posInteractable = Vector(0, 0)
 
         self.rect = pygame.Rect(self.pos.x, self.pos.y, self.width, self.height)
         self.interactableRect = pygame.Rect(0, 0, self.screen.get_height() // 25 * 3, self.screen.get_height() // 25 * 3)
@@ -24,8 +28,20 @@ class Button():
         else:
             self.color = (0, 0, 255)
 
+        if self.showRect:
+            for tile in self.tileMap.tileMapGrid:
+                if tile.rect.collidepoint(pygame.mouse.get_pos()):
+                    self.posInteractable.x = tile.pos.x + tile.size / 2 
+                    self.posInteractable.y = tile.pos.y + tile.size / 2
+                if self.interactableRect.colliderect(tile.rect) and mouseClicked:
+                    tile.type = Type.INTERACTABLE
+                    print(tile.type)
+        
+
+
     def update(self):
-        self.interactableRect.center = (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+        self.interactableRect.center = (self.posInteractable.x, self.posInteractable.y)
+        
 
     def draw(self):
 
