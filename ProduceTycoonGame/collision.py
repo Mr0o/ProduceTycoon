20d7ac2b135a3902e1b29e0921a991641d959267
Owen Smith <owen.smith1201@gmail.com>
2023-07-12 16:29:16 -0400

    Overhauled collision

--- ProduceTycoonGame/collision.py
+++ ProduceTycoonGame/collision.py
@@ -1,61 +1,45 @@
-from vectors import Vector
-from tileMap import TileMap
-from tile import Tile, Type
+from math import sqrt
 
-# this moduel contains functions for collision detection and resolution with tiles and points (as Vectors)
+# even though this game uses tiles and squares, the collision detection is being done in terms of circles
+def isCircleTouchingCircle(b1x, b1y, b1r, b2x, b2y, b2r) -> bool:
+    return ((b1x-b2x)*(b1x-b2x) + (b1y-b2y)*(b1y-b2y)) < (b1r+b2r)*(b1r+b2r)
 
-def isPosCollidingTile(pos: Vector, tile: Tile) -> bool:
-    return tile.rect.collidepoint(pos.x, pos.y)
+# wrapper function for the collision detection
+def isGuestTouchingTile(guest, tile) -> bool:
+    b1x = guest.pos.x + guest.size / 2
+    b1y = guest.pos.y + guest.size / 2
+    b1r = guest.size / 2
 
-# check if a point is colliding with a boundary tile, if so resolve by displacing the point
-def resolveBoundaryCollision(pos: Vector, tileMap: TileMap) -> Vector:
-    # get the tile that the point is colliding with
-    tile = tileMap.getTileByPos(pos)
+    b2x = tile.pos.x + tile.size / 2
+    b2y = tile.pos.y + tile.size / 2
+    b2r = tile.size / 2
 
-    # if the tile is a boundary tile
-    if tile.type == Type.BOUNDARY:
-        # if the point is colliding with the left side of the tile
-        if pos.x < tile.pos.x:
-            # resolve by displacing the point to the right
-            pos.x = tile.pos.x + tile.size
-        # if the point is colliding with the right side of the tile
-        elif pos.x > tile.pos.x + tile.size:
-            # resolve by displacing the point to the left
-            pos.x = tile.pos.x
-        # if the point is colliding with the top side of the tile
-        elif pos.y < tile.pos.y:
-            # resolve by displacing the point to the bottom
-            pos.y = tile.pos.y + tile.size
-        # if the point is colliding with the bottom side of the tile
-        elif pos.y > tile.pos.y + tile.size:
-            # resolve by displacing the point to the top
-            pos.y = tile.pos.y
+    return isCircleTouchingCircle(b1x, b1y, b1r, b2x, b2y, b2r)
 
-    return pos
 
-# prevent collisions with a boundary tile by keeping the distance between the point and the tile greater than the minimum distance
-def preventBoundaryCollision(pos: Vector, tileMap: TileMap, minDistance: int = 10) -> Vector:
-    # get the tile that the point is colliding with
-    tile = tileMap.getTileByPos(pos)
+def resolveCollision(person, tile):
+    """
+    Resolve collision between two tiles
 
-    # if the tile is a boundary tile
-    if tile.type == Type.BOUNDARY:
-        # if the point is colliding with the left side of the tile
-        if pos.x < tile.pos.x:
-            # prevent collision by displacing the point to the right
-            pos.x = tile.pos.x + tile.size + minDistance
-        # if the point is colliding with the right side of the tile
-        elif pos.x > tile.pos.x + tile.size:
-            # prevent collision by displacing the point to the left
-            pos.x = tile.pos.x - minDistance
-        # if the point is colliding with the top side of the tile
-        elif pos.y < tile.pos.y:
-            # prevent collision by displacing the point to the bottom
-            pos.y = tile.pos.y + tile.size + minDistance
-        # if the point is colliding with the bottom side of the tile
-        elif pos.y > tile.pos.y + tile.size:
-            # prevent collision by displacing the point to the top
-            pos.y = tile.pos.y - minDistance
+    - person: Tile
+    - tile: Tile
 
-    return pos
+    Collison is resolved by displacing the tiles so that they are no longer touching
+    \nReturns <person> with the updated position
+    """
+
+    #find the distance between person and tile centers
+    distance = sqrt((person.pos.x + person.size / 2 - tile.pos.x - tile.size / 2)**2 + (person.pos.y + person.size / 2 - tile.pos.y - tile.size / 2)**2)
+    #find the amount of overlap between the person and tile
+    overlap =  person.size / 2 + tile.size / 2 - distance
+
+    #prevent division by zero
+    if (distance == 0):
+        distance = 0.0001  # arbitrary small number
+
+    #displace the person
+    person.pos.x -= overlap * (person.pos.x - tile.pos.x) / distance
+    person.pos.y -= overlap * (person.pos.y - tile.pos.y) / distance
+
+    return person
     
