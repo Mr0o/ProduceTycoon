b77b58ac3fad16baac3211e60500e44e9aef0058
Owen Smith <owen.smith1201@gmail.com>
2023-07-13 11:50:14 -0400

    Overhaul physics to use pymunk

--- ProduceTycoonGame/collision.py
+++ ProduceTycoonGame/collision.py
@@ -1,36 +1,42 @@
-from math import sqrt
+# this is where we will handle all of the collision physics
+import pymunk
 
-import pygame
-from ProduceTycoonGame.vectors import Vector
+class Physics():
+    def __init__(self, tiles: list, people: list):
+        ### Init pymunk and create space
+        self.space = pymunk.Space()
+        self.space.gravity = (0.0, 0.0)
+        self.space.sleep_time_threshold = 0.3
 
-# detect collision between two pygame rects
-def isGuestTouchingTile(guest, tile) -> bool:
-    return guest.rect.colliderect(tile.rect)
+        # add tiles to the pymunk space
+        for tile in tiles:
+            self.space.add(tile.body, tile.shape)
 
+        # add people to the pymunk space
+        for person in people:
+            self.space.add(person.body, person.shape)
+        
 
-def resolveCollision(person, tile):
-    """
-    Resolve collision between two tiles
+    # run every frame
+    def update(self, dt: float):
+        self.space.step(dt)
 
-    - person: Tile
-    - tile: Tile
+    # apply a force to a person
+    def applyForce(self, person, force):
+        # apply the force to the person in pymunk space
+        person.body.apply_force_at_local_point((force.x, force.y), (0, 0))
+        # set the person's velocity to the pymunk space velocity
+        person.vel = person.body.velocity
 
-    Collison is resolved by displacing the tiles so that they are no longer touching
-    \nReturns <person> with the updated position
-    """
+    def addPerson(self, person):
+        self.space.add(person.body, person.shape)
 
-    # get the distance between the two tiles
-    distance = sqrt((person.pos.x - tile.pos.x)**2 + (person.pos.y - tile.pos.y)**2)
+    def removePerson(self, person):
+        self.space.remove(person.body, person.shape)
 
-    # get the direction of the collision
-    direction = Vector(tile.pos.x - person.pos.x, tile.pos.y - person.pos.y)
-    direction.normalize()
+    def addTile(self, tile):
+        self.space.add(tile.body, tile.shape)
 
-    # get the displacement
-    displacement = direction * distance
-
-    # apply the displacement to the person
-    person.pos -= Vector(displacement.x, displacement.y)
-
-    return person
+    def removeTile(self, tile):
+        self.space.remove(tile.body, tile.shape)
     
