29834d26d9dadfa18124c3827337c8df6a653d09
Owen Smith <owen.smith1201@gmail.com>
2023-07-12 20:32:28 -0400

    Use pygame rects to detect collision

--- ProduceTycoonGame/collision.py
+++ ProduceTycoonGame/collision.py
@@ -1,20 +1,10 @@
 from math import sqrt
 
-# even though this game uses tiles and squares, the collision detection is being done in terms of circles
-def isCircleTouchingCircle(b1x, b1y, b1r, b2x, b2y, b2r) -> bool:
-    return ((b1x-b2x)*(b1x-b2x) + (b1y-b2y)*(b1y-b2y)) < (b1r+b2r)*(b1r+b2r)
+import pygame
 
-# wrapper function for the collision detection
+# detect collision between two pygame rects
 def isGuestTouchingTile(guest, tile) -> bool:
-    b1x = guest.pos.x + guest.size / 2
-    b1y = guest.pos.y + guest.size / 2
-    b1r = guest.size / 2
-
-    b2x = tile.pos.x + tile.size / 2
-    b2y = tile.pos.y + tile.size / 2
-    b2r = tile.size / 2
-
-    return isCircleTouchingCircle(b1x, b1y, b1r, b2x, b2y, b2r)
+    return guest.rect.colliderect(tile.rect)
 
 
 def resolveCollision(person, tile):
@@ -28,8 +18,8 @@ def resolveCollision(person, tile):
     \nReturns <person> with the updated position
     """
 
-    #find the distance between person and tile centers
-    distance = sqrt((person.pos.x + person.size / 2 - tile.pos.x - tile.size / 2)**2 + (person.pos.y + person.size / 2 - tile.pos.y - tile.size / 2)**2)
+    #find the distance between the closest points on the two rects
+    distance = sqrt((person.pos.x - person.size / 2 - tile.pos.x - tile.size / 2) ** 2 + (person.pos.y - person.size / 2 - tile.pos.y - tile.size / 2) ** 2)
     #find the amount of overlap between the person and tile
     overlap =  person.size / 2 + tile.size / 2 - distance
 
@@ -38,8 +28,8 @@ def resolveCollision(person, tile):
         distance = 0.0001  # arbitrary small number
 
     #displace the person
-    person.pos.x -= overlap * (person.pos.x - tile.pos.x) / distance
-    person.pos.y -= overlap * (person.pos.y - tile.pos.y) / distance
+    person.pos.x += (person.pos.x - tile.pos.x) / distance * overlap
+    person.pos.y += (person.pos.y - tile.pos.y) / distance * overlap
 
     return person
     
