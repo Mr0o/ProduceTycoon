259dcdb93c46431da08e8cf4755d695bbb0c9c61
Owen Smith <owen.smith1201@gmail.com>
2023-07-15 21:13:13 -0400

    Circle based collison physics resolution

---
+++ ProduceTycoonGame/collision.py
@@ -0,0 +1,74 @@
+from ProduceTycoonGame.vectors import Vector
+from ProduceTycoonGame.guest import Guest
+from ProduceTycoonGame.tile import Tile
+
+from math import sqrt
+
+def isGuestTouchingTile(guest: Guest, tile: Tile) -> bool:
+    """
+    Returns True if the guest is touching the tile
+    """
+
+    b1x = guest.pos.x + guest.size/2
+    b1y = guest.pos.y + guest.size/2
+    b2x = tile.pos.x + tile.size/2
+    b2y = tile.pos.y + tile.size/2
+    
+    guestRadius = guest.size/2
+    tileRadius = tile.size/2
+
+    return ((b1x-b2x)*(b1x-b2x) + (b1y-b2y)*(b1y-b2y)) < (guestRadius+tileRadius)**2
+
+
+def resolveCollision(guest: Guest, tile: Tile) -> Guest:
+    """
+    Resolves the collision between the guest and the tile\n
+    Collisions are circle vs circle\n
+    So the guest and tile are treated like circles\n
+    Returns the guest with the new updated velocity and position
+    """
+
+    guestCenterX = guest.pos.x + guest.size/2
+    guestCenterY = guest.pos.y + guest.size/2
+    tileCenterX = tile.pos.x + tile.size/2
+    tileCenterY = tile.pos.y + tile.size/2
+
+    guestRadius = guest.size/2
+    tileRadius = tile.size/2
+
+    #find the distance between ball and peg centers
+    distance = sqrt((guestCenterX - tileCenterX)**2 + (guestCenterY - tileCenterY)**2)
+    #find the amount of overlap between the ball and peg
+    overlap =  1.0 * (guestRadius + tileRadius - distance)
+
+    #prevent division by zero
+    if (distance == 0):
+        distance = 0.0001  # arbitrary small number
+
+    #displace the ball
+    guest.pos.x += overlap * (guest.pos.x - tile.pos.x) / distance
+    guest.pos.y += overlap * (guest.pos.y - tile.pos.y) / distance
+
+    ## workout dynamic collisions
+    #normal
+    nx = (guest.pos.x - tile.pos.x) / distance
+    ny = (guest.pos.y - tile.pos.y) / distance
+
+    #tangent
+    tx = -ny
+    ty = nx
+
+    #dot product tangent
+    dpTan = guest.vel.x * tx + guest.vel.y * ty
+
+    #dot product normal
+    dpNorm1 = guest.vel.x * nx + guest.vel.y * ny
+
+    #conservation of momentum in 1D
+    m = (dpNorm1 * (guest.mass - tile.mass) + 2.0 * tile.mass * 0) / (guest.mass + tile.mass)
+
+    #update velocity of the ball
+    guest.vel.x = tx * dpTan + nx * m
+    guest.vel.y = ty * dpTan + ny * m   
+
+    return guest
