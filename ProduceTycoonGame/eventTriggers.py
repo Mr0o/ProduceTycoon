fb329a82a293591ebe8afbac9f3d8533d7404577
Owen Smith <owen.smith1201@gmail.com>
2023-07-18 02:12:50 -0400

    Changed name: timer -> seconds

--- ProduceTycoonGame/eventTriggers.py
+++ ProduceTycoonGame/eventTriggers.py
@@ -1,27 +1,27 @@
 import time
 
 class TimedEvent:
-    def __init__(self, timer = None):
+    def __init__(self, seconds: int = 0):
         self.timeStamp = time.time() # time that this object was created
 
         self.currentTime = time.time()
-        self.triggerTime = self.currentTime + timer
+        self.triggerTime = self.currentTime + seconds
 
         self.timeRemaining = self.triggerTime - self.currentTime
 
-        # if the timer is None then it has not been set and is therfore not active
+        # if the seconds is None then it has not been set and is therfore not active
         self.isActive = False
-        if timer:
+        if seconds:
             self.isActive = True
 
         self.isTriggered = False
     
 
-    def setTimer(self, timer):
+    def setTimer(self, seconds: int):
         self.isActive = True
         self.isTriggered = False
         self.currentTime = time.time()
-        self.triggerTime = self.currentTime + timer
+        self.triggerTime = self.currentTime + seconds
 
 
     def cancleTimer(self):
