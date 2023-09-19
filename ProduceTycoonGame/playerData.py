a57cf3dbdf72c38cd5707ad2e3d6dce273dc5dc0
Lukas Tackett <luetack@gmail.com>
2023-09-19 01:16:40 -0400

    New PlayerData handler class

---
+++ ProduceTycoonGame/playerData.py
@@ -0,0 +1,15 @@
+import pygame
+import json
+
+class PlayerData:
+    money = 1000
+    amountWatermelons = 0
+    amountApples = 0
+    amountBananas = 0
+    amountTomatoes = 0
+
+    def loadData():
+        pass
+
+    def saveData():
+        pass
