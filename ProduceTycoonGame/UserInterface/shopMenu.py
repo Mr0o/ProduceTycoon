4fe27f26d31eae12bac73180dec843cc483082d5
Lukas Tackett <luetack@gmail.com>
2023-09-19 16:25:39 -0400

    One main buy function

--- ProduceTycoonGame/UserInterface/shopMenu.py
+++ ProduceTycoonGame/UserInterface/shopMenu.py
@@ -6,6 +6,15 @@ from ProduceTycoonGame.UserInterface.text import Text
 from ProduceTycoonGame.playerData import PlayerData
 from ProduceTycoonGame.produce import Produce, Watermelon, Bananas, Apples, Tomatoes
 
+# ---------- Helper Functions ---------- 
+def buy(PRODUCE: Produce):
+        if PlayerData.money < PRODUCE.buy:
+            print("---- Insufficient funds ----")
+            return
+        PlayerData.money -= PRODUCE.buy
+        PRODUCE.amount += 1
+        print(f"---- Purchased {PRODUCE.name} ----")
+
 class ShopMenu():
     # Variables
     pos: Vector
@@ -18,55 +27,37 @@ class ShopMenu():
     # Static Variables
     buttons = []
     screen = pygame.Surface((0, 0))
+    x = 0
+    y = 0
 
+    # ---------- Static methods ---------- 
+    @staticmethod
+    def setScreen(screen):
+        ShopMenu.screen = screen
+
+    def defineXandY(self):
+        ShopMenu.x = self.pos.x + 10
+        ShopMenu.y = self.pos.y + 10
+
+    # ---------- Constructor ----------
+    def __init__(self, pos: Vector, width: int, height: int):
+        self.pos = pos
+        self.width = width
+        self.height = height
+        self.color = (122, 238, 186)
+
+        self.rect = pygame.Rect((self.pos.x, self.pos.y), (self.width, self.height))
+
+        ShopMenu.defineXandY(self)
+
+        ShopMenu.buttons = self.createButtons()
+
+    # ---------- Helpers ----------
     def openGUI(self):
         self.active = True
 
     def exitGUI(self):
         self.active = False
-
-    def buyWatermelon(self):
-        if PlayerData.money < 100:
-            print("---- Insufficient funds ----")
-            return
-        PlayerData.money -= Watermelon.buy
-        Watermelon.amount += 1
-        print("---- Purchased watermelon ----")
-
-    def buyBananas(self):
-        if PlayerData.money < 100:
-            print("---- Insufficient funds ----")
-            return
-        PlayerData.money -= Bananas.buy
-        Bananas.amount += 1
-        print("---- Purchased bananas ----")
-
-    def buyApples(self):
-        if PlayerData.money < 100:
-            print("---- Insufficient funds ----")
-            return
-        PlayerData.money -= Apples.buy
-        Apples.amount += 1
-        print("---- Purchased apples ----")
-
-    def buyTomatoes(self):
-        if PlayerData.money < 100:
-            print("---- Insufficient funds ----")
-            return
-        PlayerData.money -= Tomatoes.buy
-        Tomatoes.amount += 1
-        print("---- Purchased tomatoes ----")
-
-    # Static methods
-    @staticmethod
-    def setScreen(screen):
-        ShopMenu.screen = screen
-
-    x = 0
-    y = 0
-    def defineXandY(self):
-        ShopMenu.x = self.pos.x + 10
-        ShopMenu.y = self.pos.y + 10
 
     def createButton(self, name: str, width: int, height: int, func: callable):
         button = Button(Vector(ShopMenu.x, ShopMenu.y), name, width, height, func)
@@ -85,27 +76,15 @@ class ShopMenu():
         buttonWidth = int((self.width - offset) / size)
         buttonHeight = int((self.height - offset) / size)
         return [
-            self.createButton('Watermelon', buttonWidth, buttonHeight, self.buyWatermelon),
-            self.createButton('Bananas', buttonWidth, buttonHeight, self.buyBananas),
-            self.createButton('Apples', buttonWidth, buttonHeight, self.buyApples),
-            self.createButton('Tomatoes', buttonWidth, buttonHeight, self.buyTomatoes),
+            self.createButton(Watermelon.name, buttonWidth, buttonHeight, lambda: buy(Watermelon)),
+            self.createButton(Bananas.name, buttonWidth, buttonHeight, lambda: buy(Bananas)),
+            self.createButton(Apples.name, buttonWidth, buttonHeight, lambda: buy(Apples)),
+            self.createButton(Tomatoes.name, buttonWidth, buttonHeight, lambda: buy(Tomatoes)),
 
             self.createButtonWithPos('X', self.pos, 20, 20, self.exitGUI)
         ]
 
-    def __init__(self, pos: Vector, width: int, height: int):
-        self.pos = pos
-        self.width = width
-        self.height = height
-        self.color = (122, 238, 186)
-
-        self.rect = pygame.Rect((self.pos.x, self.pos.y), (self.width, self.height))
-
-        ShopMenu.defineXandY(self)
-
-        ShopMenu.buttons = self.createButtons()
-
-    # Main methods
+    # ---------- Main methods ---------- 
     def events(self):
         if self.active:
             for button in reversed(ShopMenu.buttons):
