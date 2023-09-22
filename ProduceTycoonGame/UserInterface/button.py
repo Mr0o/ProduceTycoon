15c187beda8c71e0ca72cb412ff9a2befa297125
Lukas Tackett <luetack@gmail.com>
2023-09-21 23:28:54 -0400

    Grammar

--- ProduceTycoonGame/UserInterface/button.py
+++ ProduceTycoonGame/UserInterface/button.py
@@ -4,35 +4,6 @@ from ProduceTycoonGame.events import eventOccured
 from ProduceTycoonGame.vectors import Vector
 from ProduceTycoonGame.UserInterface.text import Text
 
-#class Button():
-#    static_screen = pygame.Surface((0, 0))
-#    def __init__(self, pos: Vector, name: str, width: int, height: int, color: tuple[int, int, int]= (200, 110, #110)):
-#        self.pos = pos
-#        self.name = name
-#        self.width = width
-#        self.height = height
-#        self.color = color
-#        
-#        self.textRenderer = Text(Button.static_screen, self.pos, self.width, self.height, self.name)
-#        self.rect = pygame.Rect((self.pos.x, self.pos.y), (self.width, self.height))
-#
-#        self.isSelected = False
-#        self.hidden = False
-#
-#    
-#    
-#    def events(self, eventOccured("leftMouseDown"): bool):
-#        
-#
-#    def draw(self):
-#        if self.hidden:
-#            return
-#        pygame.draw.rect(Button.static_screen, self.color, self.rect)
-#        pygame.draw.rect(Button.static_screen, (0, 0, 0), self.rect, 2)
-#        self.textRenderer.draw()
-
-
-
 class ButtonInfo():
     pos: Vector
     name: str
