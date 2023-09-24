c75252bb85a5cb6ee72bc2696b89e9963de662da
Owen Smith <owen.smith1201@gmail.com>
2023-09-23 23:26:56 -0400

    Added dependency installer to main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Check dependencies
+from dependsInstaller import isDependenciesInstalled, installDependencies
+if not isDependenciesInstalled():
+    installDependencies()
+
+# Run the game
 from ProduceTycoonGame.game import Game
 
 if __name__ == '__main__':
