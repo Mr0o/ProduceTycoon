17ee1dce8cc765adb9aa2f039e4cdf1d9f8c703b
Owen Smith <owen.smith1201@gmail.com>
2023-10-05 20:08:20 -0400

    Added Python version check

--- dependsInstaller.py
+++ dependsInstaller.py
@@ -2,6 +2,15 @@
 # Automatically install the missing dependencies
 
 def isDependenciesInstalled() -> bool:
+    # first check for correct python version
+    import sys
+    if sys.version_info < (3, 10):
+        print(f"\x1b[31mERROR: Python {sys.version_info[0]}.{sys.version_info[1]} is not supported!\x1b[0m")
+        print("\n\x1b[33mPlease install Python 3.10 or higher.\x1b[0m")
+
+        exit()
+    
+    # try to import dependencies
     try:
         import pygame
 
@@ -34,8 +43,8 @@ def installDependencies() -> None:
     
     # if the dependencies are still not installed
     if not isDependenciesInstalled():
-        print("ERROR: Failed to install dependencies using pip. \n ")
-        print("Please install the dependencies manually using the command:\n ")
+        print("ERROR: Failed to install dependencies using pip.")
+        print("Please install the dependencies manually using the command: \n")
         print(f"\t{sys.executable} -m pip install -r requirements.txt \n")
         
         exit()
