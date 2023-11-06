41fc01f969544729dddfc433bc3c1c2e4200e7c8
owen smith <owen.smith1201@gmail.com>
2023-11-06 18:30:58 -0500

    Use asserts instead of print statements for errors Prep for tests

--- dependsInstaller.py
+++ dependsInstaller.py
@@ -4,11 +4,10 @@
 def isDependenciesInstalled() -> bool:
     # first check for correct python version
     import sys
-    if sys.version_info < (3, 10):
-        print(f"\x1b[31mERROR: Python {sys.version_info[0]}.{sys.version_info[1]} is not supported!\x1b[0m")
-        print("\n\x1b[33mPlease install Python 3.10 or higher.\x1b[0m")
 
-        exit()
+    assert not sys.version_info < (3, 10), f"""
+        \x1b[31mERROR: Python {sys.version_info[0]}.{sys.version_info[1]} is not supported!\x1b[0m
+        \x1b[33mPlease install Python 3.10 or higher.\x1b[0m"""
     
     # try to import dependencies
     try:
@@ -21,8 +20,7 @@ def isDependenciesInstalled() -> bool:
 
 
 def installDependencies() -> None:
-    # Try running 'pip install -r requirements.txt'
-    # using variouse methods
+    # Try running 'python -m pip install -r requirements.txt'
 
     print("Installing dependencies... \n")
 
@@ -36,18 +34,15 @@ def installDependencies() -> None:
         time.sleep(1)
     except subprocess.CalledProcessError:
         # pip is likely not installed
-        print("ERROR: Failed to install dependencies using pip. \n")
-        print("Please make sure pip is installed\n")
-
-        exit()
+        assert False, """
+            ERROR: Failed to install dependencies using pip. \n
+            Please make sure pip is installed\n"""
     
-    # if the dependencies are still not installed
-    if not isDependenciesInstalled():
-        print("ERROR: Failed to install dependencies using pip.")
-        print("Please install the dependencies manually using the command: \n")
-        print(f"\t{sys.executable} -m pip install -r requirements.txt \n")
-        
-        exit()
+    # dependencies must be installed to continue
+    assert not isDependenciesInstalled(), f"""
+        ERROR: Failed to install dependencies using pip.
+        Please install the dependencies manually using the command: \n
+        \t{sys.executable} -m pip install -r requirements.txt \n"""
     
 
 
